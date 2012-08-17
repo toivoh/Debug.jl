5ad66e54596b3d9e9c6c8a61d9e1c21947e36167
Toivo Henningsson <toivo@control.lth.se>
2012-08-17 09:39:22 +0200

    Change test_debugeval to use relative line numbers.

--- test/test_debugeval.jl
+++ test/test_debugeval.jl
@@ -5,43 +5,44 @@ module TestDebugEval
 import Base.*
 import Debug.*
 
-
-@debug function f(n)
-    x = 0      # this must be line #10
-    for k=1:n                      #11
-        x += k                     #12
+const f_line = 9 # line number of the next line:
+@debug function f(n) # this must be line number f_line
+    x = 0     # line number f_line + 1
+    for k=1:n                      # 2
+        x += k                     # 3
+    end                            # 4
+    x = x*x                        # 5
+    let y=2x                       # 6
+        y                          # 7
+    end                            # 8
+    try                            # 9
+        error("throw something")   #10
+    catch e                        #11
+        x                          #12
     end                            #13
-    x = x*x                        #14
-    let y=2x                       #15
-        y                          #16
-    end                            #17
-    try                            #18
-        error("throw something")   #19
-    catch e                        #20
-        x                          #21
-    end                            #22
-    [begin x^2 end for x=1:3]      #23
-    g = (x, y)->begin              #24
-        x = -1                     #25
-        a, b = 1, 2                #26
-        x                          #27
-    end                            #28
-    g(n, n)                        #29
-    x                              #30
+    [begin x^2 end for x=1:3]      #14
+    g = (x, y)->begin              #15
+        x = -1                     #16
+        a, b = 1, 2                #17
+        x                          #18
+    end                            #19
+    g(n, n)                        #20
+    x                              #21
 end
 
-function debug_hook(line::Int, file, scope::Scope) 
+function debug_hook(line::Int, file, scope::Scope)
+    line -= f_line
     print(line, ":")
 
-    if (line == 11) debug_eval(scope, :(x += 1)) end
+    if (line ==  2) debug_eval(scope, :(x += 1)) end
 
-    if (line >  10) debug_eval(scope, :(print("\tx = ", x))) end
-    if (line == 12) debug_eval(scope, :(print("\tk = ", k))) end
-    if (line == 16) debug_eval(scope, :(print("\ty = ", y))) end
-    if (line == 21) debug_eval(scope, :(print("\te = ", e))) end
-    if (line >  24) debug_eval(scope, :(print("\tg = ", g))) end
-    if (25 <= line <= 27) debug_eval(scope, :(print("\ty = ", y))) end
-    if (line == 27) debug_eval(scope, :(print("\ta = ", a, "\tb = ", b, ))) end
+    if (line >   1) debug_eval(scope, :(print("\tx = ", x))) end
+    if (line ==  3) debug_eval(scope, :(print("\tk = ", k))) end
+    if (line ==  7) debug_eval(scope, :(print("\ty = ", y))) end
+    if (line == 12) debug_eval(scope, :(print("\te = ", e))) end
+    if (line >  15) debug_eval(scope, :(print("\tg = ", g))) end
+    if (16 <= line <= 18) debug_eval(scope, :(print("\ty = ", y))) end
+    if (line == 18) debug_eval(scope, :(print("\ta = ", a, "\tb = ", b, ))) end
     println()
 end
 
