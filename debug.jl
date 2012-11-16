c9c3fc7ddf0fc9a1875f361696ee98f670a0f82f
Toivo Henningsson <toivo@control.lth.se>
2012-11-16 13:06:15 +0100

    Added back minimal instrument().

--- debug.jl
+++ debug.jl
@@ -44,5 +44,33 @@ function replicate_last{T}(v::Vector{T}, n)
 end
 
 
+# ---- instrument -------------------------------------------------------------
+
+
+instrument(ex) = instrument("", ex)
+
+instrument(file::String, ex) = ex
+function instrument(file::String, ex::Expr)
+    head, args = ex.head, ex.args
+    if contains([:line, :quote, :top, :macrocall, :type], head)
+        ex
+    elseif head === :block
+        code = {}
+        for arg in args
+            if is_linenumber(arg)
+                line = get_linenumber(arg)
+                if is_expr(arg, :line, 2); file = arg.args[2]; end
+                
+                push(code, arg)
+                push(code, :($(quot(trap))($line, $(quot(file)))) )
+            else
+                push(code, instrument(arg))
+            end
+        end
+        expr(head, code)
+    else
+        expr(head, {instrument(arg) for arg in args})
+    end
+end
 
 end # module
---
+++ test/test_instrument.jl
@@ -0,0 +1,20 @@
+load("debug.jl")
+
+module TestInstrument
+using Base, Debug
+import Debug.trap
+
+trap(line, file) = println("trap: line = $line, file = $(repr(file))")
+
+code = quote
+    for i=1:3
+        println(i)
+    end
+end
+
+icode = instrument(code)
+println(icode)
+
+eval(icode)
+
+end
