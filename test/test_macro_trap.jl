1f59fda9579ada0515c61287ecd0cb28788c80d6
Toivo Henningsson <toivo@control.lth.se>
2012-11-28 16:39:21 +0100

    Add a test that @debug can trap inside an expanded macro. Closes #9

---
+++ test/test_macro_trap.jl
@@ -0,0 +1,25 @@
+include(find_in_path("Debug.jl"))
+
+module TestMacroTrap
+using Base, Debug
+
+function trap(loc::Loc, scope::Scope)
+    if loc.line == 15
+        @assert scope[:z] == 3
+        scope[:z] = 4
+    end
+end
+
+macro m()
+    esc(quote
+        z  # must be line 15!
+    end)
+end
+
+@debug trap let
+    z = 3
+    @m
+    @assert z == 4
+end
+
+end # module
--- test/tests.jl
+++ test/tests.jl
@@ -1,4 +1,5 @@
 include("test_decorate.jl")
 include("test_graft.jl")
+include("test_macro_trap.jl")
 include("test_trap.jl")
 include("test_debugeval.jl")
