fc37ca7fe7a37ad944f7fccc1df6b72e646528d2
Toivo Henningsson <toivo@control.lth.se>
2012-12-06 13:31:34 +0100

    Tidy up Meta

--- src/Meta.jl
+++ src/Meta.jl
@@ -7,14 +7,14 @@ module Meta
 using Base, AST
 export quot, is_expr
 export isblocknode
+
 quot(ex) = expr(:quote, {ex})
 
-is_expr(ex::Ex, head)  = (headof(ex) === head)
+is_expr(ex::Ex, head)          = headof(ex) === head
 is_expr(ex::Ex, heads::Set)    = has(heads, headof(ex))
 is_expr(ex::Ex, heads::Vector) = contains(heads, headof(ex))
-is_expr(ex,     head)  = false
+is_expr(ex,     head)          = false
 is_expr(ex,     head, n::Int)  = is_expr(ex, head) && nargsof(ex) == n
-
 
 isblocknode(node) = is_expr(node, :block)
 
