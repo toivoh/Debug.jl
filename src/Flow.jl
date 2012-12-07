9ea987f39a833b9b7df53881fabc2619c52fac06
Toivo Henningsson <toivo@control.lth.se>
2012-12-07 17:15:37 +0100

    Temp

--- src/Flow.jl
+++ src/Flow.jl
@@ -10,6 +10,7 @@ export @bp, BPNode, DBState
 export continue!, singlestep!, stepover!, stepout!
 
 
+## BreakPoint ##
 type BreakPoint; end
 typealias BPNode Node{BreakPoint}
 is_emittable(::BPNode) = false
@@ -17,6 +18,72 @@ is_emittable(::BPNode) = false
 macro bp()
     Node(BreakPoint())
 end
+
+is_trap(::BPNode)   = true
+is_trap(::Event)    = true
+is_trap(node::Node) = is_evaluable(node) && isblocknode(parentof(node))
+
+
+## Frame ##
+type Frame
+    node::Node
+    scope::Scope
+end
+isequal(f1::Frame, f2::Frame) = (f1.node === f2.node && f1.scope === f2.scope)
+
+
+## Cond ##
+abstract Cond
+
+type Continue   <: Cond; end
+type SingleStep <: Cond; end
+
+type ContinueInside <: Cond; frame::Frame; outside::Cond; end
+type StepOver       <: Cond; end
+
+does_trap(::SingleStep)     = true
+does_trap(::Continue)       = false
+does_trap(::ContinueInside) = false
+does_trap(::StepOver)       = true
+
+leave(cond::ContinueInside, f::Frame) = (cond.frame == f ? cond.outside : cond)
+
+enter(cond::StepOver, frame::Frame) = ContinueInside(frame, cond)
+leave(cond::StepOver, frame::Frame) = SingleStep()
+
+enter(cond::Cond, ::Frame) = cond
+leave(cond::Cond, ::Frame) = cond
+
+
+## DBState ##
+type DBState
+    cond::Cond
+    DBState() = new(Continue(),{})
+end
+
+continue!(  s::DBState) = (s.cond = Continue())
+singlestep!(s::DBState) = (s.cond = SingleStep())
+stepover!(  s::DBState) = (s.cond = StepOver())
+function stepout!(st::DBState, node::Node, s::Scope)
+    st.cond = ContinueInside(enclosing_frame(Frame(node, s)), SingleStep())
+end
+
+enclosing_frame(f::Frame) = frameof(parent_frame(f))
+function parent_frame(f::Frame)
+    node = parentof(f.node)
+    Frame(node, introduces_scope(node) ? f.scope.parent : f.scope)
+end
+
+
+trap(st::DBState, ::BPNode, s::Scope) = (singlestep!(st); false)
+trap(st::DBState, e::Enter, s::Scope) = (st.cond=enter(s.cond, Frame(e.node,s))
+trap(st::DBState, e::Leave, s::Scope) = (st.cond=leave(s.cond, Frame(e.node,s))
+trap(st::DBState, n::Node,  s::Scope) = does_trap(n)
+
+
+
+
+
 
 
 is_trap(::BPNode)   = true
