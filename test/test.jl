7ad871317da726b650e8718705d11c552296a5ec
Toivo Henningsson <toivo.h.h@gmail.com>
2013-03-05 20:29:57 +0100

    Make test.jl debug code inside of a function, to give it source file info

--- test/test.jl
+++ test/test.jl
@@ -6,15 +6,18 @@ println(Debug.UI.helptext)
 println()
 println("Type an expression to evaluate it in the current scope.")
 
-@debug let
-    x, y = 0, 1
-    @bp
-    go_on = true
-    while go_on
-        println("$x, $y")
-        @bp x > 100
-        x, y = y, x+y
+@debug begin
+    function test()
+        x, y = 0, 1
+        @bp
+        go_on = true
+        while go_on
+            println("$x, $y")
+            @bp x > 100
+            x, y = y, x+y
+        end
     end
+    test()
 end
 
 end # module
